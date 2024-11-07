269e9e5be843b3184e1a2dcdc73d392bb01ff3da
Brahmaiah.T <bramhithangedupalli@gmail.com>
2024-11-07 19:58:17 +0530

    instances creating

--- creating instances.sh
+++ creating instances.sh
@@ -15,5 +15,5 @@ do
     instance_type="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --count 1 --instance-type t2.micro --security-group-ids sg-084393c1fa626268c --region us-east-1
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --count 1 --instance-type stance_type --security-group-ids sg-084393c1fa626268c --region us-east-1
 done
