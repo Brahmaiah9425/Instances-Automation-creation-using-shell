b505a9179b8bc0bd9bbfc268f65ff328e9dcf787
Brahmaiah.T <bramhithangedupalli@gmail.com>
2024-11-07 19:51:06 +0530

    instances creating

--- creating instances.sh
+++ creating instances.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --count 1 --instance-type t2.micro --security-group-ids sg-084393c1fa626268c --region us-east-1
+
+AMI=ami-0b4f379183e5706b9 
+
+SG_ID=sg-084393c1fa626268c
+
+instances=("mongodb" " cart"  "payments"  "radis"  "web"  "user"  "mysql"  "radditmq"  "catalogue"  "shipping")
+
+for i "${instances[@]}"
+do 
+    if [$i == "mongodb"] | [$i == "web"] | [$i == "shipping"]
+    then
+    instance_type="t3.small"
+    else
+    instance_type="t2.micro"
+    fi
+
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --count 1 --instance-type t2.micro --security-group-ids sg-084393c1fa626268c --region us-east-1
+done
