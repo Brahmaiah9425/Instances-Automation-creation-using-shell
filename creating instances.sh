3be93c6d96a37eac56f9bf7c1ec11250712ab8cf
Brahmaiah.T <bramhithangedupalli@gmail.com>
2024-11-07 20:07:45 +0530

    instances creating

--- creating instances.sh
+++ creating instances.sh
@@ -10,10 +10,10 @@ for i in "${instances[@]}"
 do 
     if [$i == "mongodb"] | [$i == "web"] | [$i == "shipping"]
     then
-    instance_type="t3.small"
+        INSTANCE_TYPE="t3.small"
     else
-    instance_type="t2.micro"
+        INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --count 1 --instance-type $istance_type --security-group-ids sg-084393c1fa626268c --region us-east-1
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-084393c1fa626268c --region us-east-1
 done
